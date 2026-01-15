7c8f6df5d7e73c4cd3cb66d42a7a46b43d452939
Kesha768 <v_garkusha@internet.ru>
2026-01-15 13:30:49 +0300

    first base commit

--- shared/components/shared/header/header.tsx
+++ shared/components/shared/header/header.tsx
@@ -8,6 +8,11 @@ import { Search } from "./search";
 import { Logo_H_Desctop } from "../../svg/logo_H_Desctop";
 import { CardsProps } from "@/shared/interface/cards";
 import { RegionLink } from "../region-link";
+import { Logo_Mob } from "../../svg/logo-mob";
+import { SearchIcon } from "../../svg/search";
+import { Burger } from "../../svg/burger";
+import { Cross } from "../../svg/cross";
+
 
 interface Props {
   data: CardsProps[];
@@ -15,9 +20,11 @@ interface Props {
 
 export const Header: React.FC<Props> = ({data}) => {
   const [isSearchOpen, setIsSearchOpen] = React.useState(false);
+  const [isBurgerOpen, setIsBurgerOpen] = React.useState(false);
   return (
     <header className="header">
       <div className="header__row">
+        <Logo_Mob className="header__logo-mob"/>
         <Search
           isOpen={isSearchOpen}
           onOpen={() => setIsSearchOpen(true)}
@@ -32,8 +39,36 @@ export const Header: React.FC<Props> = ({data}) => {
             <Select />
           </>
         )}
+        <div className="header__info-mob">
+          <div className="header__mob-search">
+            <div className="header__mob-search-icon">
+              <SearchIcon />
+            </div>
+            <input 
+              type="text" 
+              placeholder="Найти в новостях" 
+              className="header__mob-input"
+            />
+          </div>
+          <div 
+            className="header__burger-circle" 
+            onClick={() => setIsBurgerOpen(!isBurgerOpen)}
+          >
+            <Burger />
+          </div>
+        </div>
       </div>
       <Bar Topics={data}/>
+      {isBurgerOpen && (
+        <div className="burger-block">
+          <div className="header__burger-circle cross" onClick={() => setIsBurgerOpen(false)}>
+            <Cross />
+          </div>
+          <div className="header__burger-line"></div>
+          <h6 className="header__burger-title">Регион</h6>
+          <Select />
+        </div>
+      )}
     </header>
   );
 };
